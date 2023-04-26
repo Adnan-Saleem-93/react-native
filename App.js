f6e9adf2c0d3dbe893a6a4e3b8e86d3f0b938642
Adnan Saleem <adnan13893@gmail.com>
2023-04-26 16:26:48 +0500

    initial commit

--- App.js
+++ App.js
@@ -1,20 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		<View style={styles.container}>
+			<Text style={styles.text}>First React Native Project!</Text>
+			<StatusBar style="auto" animated={true} backgroundColor="red" />
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: "lightblue",
+		alignItems: "center",
+		justifyContent: "center",
+	},
+	text: {
+		color: "purple",
+		fontSize: 28,
+		fontWeight: 800,
+	},
 });
